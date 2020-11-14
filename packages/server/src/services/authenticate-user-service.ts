4e24bbe8356b2fd8f36093b9fff0cf5291a757ae
flexcommits <leeweiming524@gmail.com>
2020-11-14 14:23:52 -0300

    feat: validate user credentials

---
+++ packages/server/src/services/authenticate-user-service.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+
+import { User } from '../models/user';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+}
+
+export class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    return { user };
+  }
+}
