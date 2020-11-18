e5dcddce98e9866e2eb0af1bd7e0ddb75709c680
flexcommits <leeweiming524@gmail.com>
2020-11-18 13:51:55 -0300

    feat: import and use cors

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import { AppError } from './errors/app-error';
@@ -14,6 +15,7 @@ const SERVER_PORT = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
