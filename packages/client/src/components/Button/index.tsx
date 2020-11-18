7119aa34e6fb92cb0949a8476d30a7ae00a7950c
flexcommits <leeweiming524@gmail.com>
2020-11-18 09:14:34 -0300

    feat: add Input and Button components

---
+++ packages/client/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+import { Container } from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container type="button" {...rest}>
+    {children}
+  </Container>
+);
