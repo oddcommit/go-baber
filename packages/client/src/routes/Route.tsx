b73e1323d3f6ff6597467a40b28abc68d42b196d
flexcommits <leeweiming524@gmail.com>
2020-11-18 22:00:55 -0300

    feat: add personal Route component
    Added a custom Route component to allow redirection of private routes.

---
+++ packages/client/src/routes/Route.tsx
@@ -0,0 +1,39 @@
+import React, { ReactSVG } from 'react';
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDOMRoute,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+export const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
