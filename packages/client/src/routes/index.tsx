beb09b94fff2769ddc26c920cd08c40e61267f50
flexcommits <leeweiming524@gmail.com>
2020-11-18 22:00:34 -0300

    feat: add application routing

---
+++ packages/client/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import { Route } from './Route';
+
+import { SignIn } from '../pages/SignIn';
+import { SignUp } from '../pages/SignUp';
+
+import { Dashboard } from '../pages/Dashboard';
+
+export const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+  </Switch>
+);
