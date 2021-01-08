0b371740296b5cd8646338bc21a4855bccd4ba75
flexcommits <leeweiming524@gmail.com>
2021-01-08 08:21:55 -0300

    test: add authenticate erros test

--- packages/server/src/modules/users/services/authenticate-user-service.spec.ts
+++ packages/server/src/modules/users/services/authenticate-user-service.spec.ts
@@ -1,4 +1,5 @@
 // import { AppError } from '@shared/errors/app-error';
+import { AppError } from '@shared/errors/app-error';
 import { FakeUsersRepository } from '../repositories/fakes/fake-users-repository';
 import { FakeHashProvider } from '../providers/hash-provider/fakes/fake-hash-provider';
 import { AuthenticateUserService } from './authenticate-user-service';
@@ -32,5 +33,50 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
