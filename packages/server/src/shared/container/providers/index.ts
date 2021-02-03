65c6daf7bc6e17d8b7e310d121f619418cfd578b
flexcommits <leeweiming524@gmail.com>
2021-02-03 18:41:07 -0300

    feat: add mail template provider DI

--- packages/server/src/shared/container/providers/index.ts
+++ packages/server/src/shared/container/providers/index.ts
@@ -3,7 +3,23 @@ import { container } from 'tsyringe';
 import { IStorageProvider } from './storage-provider/models/interface-storage-provider';
 import { DiskStorageProvider } from './storage-provider/implementations/disk-storage-provider';
 
+import { IMailProvider } from './mail-provider/models/interface-mail-provider';
+import { EtherealMailProvider } from './mail-provider/implementations/ethereal-mail-provider';
+
+import { IMailTemplateProvider } from './mail-template-provider/models/interface-mail-template-provider';
+import { HandlebarsMailTemplateProvider } from './mail-template-provider/implementations/handlebars-mail-template-providers';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve(EtherealMailProvider),
 );
