54358b8661d20e30e516659df1ecb301bd316873
flexcommits <leeweiming524@gmail.com>
2021-02-03 16:36:31 -0300

    feat: add user tokens

--- packages/server/src/modules/users/repositories/interface-user-tokens-repository.ts
+++ packages/server/src/modules/users/repositories/interface-user-tokens-repository.ts
@@ -2,4 +2,5 @@ import { UserToken } from '../infra/typeorm/entities/user-token';
 
 export interface IUserTokensRepository {
   generate(user_id: string): Promise<UserToken>;
+  findByToken(token: string): Promise<UserToken | undefined>;
 }
