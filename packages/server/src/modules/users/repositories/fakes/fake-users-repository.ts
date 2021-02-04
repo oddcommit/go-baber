b32a29b39122c4d6f20f108033636ec68336656b
flexcommits <leeweiming524@gmail.com>
2021-02-04 16:42:01 -0300

    feat: add findAllProviders to fake repository

--- packages/server/src/modules/users/repositories/fakes/fake-users-repository.ts
+++ packages/server/src/modules/users/repositories/fakes/fake-users-repository.ts
@@ -2,6 +2,7 @@ import { uuid } from 'uuidv4';
 
 import { IUsersRepository } from '@modules/users/repositories/interface-users-repository';
 import { ICreateUserDTO } from '@modules/users/dtos/interface-create-user-dto';
+import { IFindAllProvidersDTO } from '@modules/users/dtos/interface-find-all-providers-dto';
 
 import { User } from '../../infra/typeorm/entities/user';
 
@@ -16,6 +17,18 @@ export class FakeUsersRepository implements IUsersRepository {
   public async findByEmail(email: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.email === email);
     return findUser;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
