4503684d2b9be2b780b74746f1266bbfcf7e33f4
flexcommits <leeweiming524@gmail.com>
2021-02-04 16:42:35 -0300

    feat: add findAllProviders to interface

--- packages/server/src/modules/users/repositories/interface-users-repository.ts
+++ packages/server/src/modules/users/repositories/interface-users-repository.ts
@@ -1,7 +1,9 @@
+import { IFindAllProvidersDTO } from '@modules/users/dtos/interface-find-all-providers-dto';
 import { User } from '../infra/typeorm/entities/user';
 import { ICreateUserDTO } from '../dtos/interface-create-user-dto';
 
 export interface IUsersRepository {
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
