e72ce636353b3233ba2a90009c381ae93d3e64ee
flexcommits <leeweiming524@gmail.com>
2021-02-05 10:17:09 -0300

    feat: add list providers appointments

--- packages/server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ packages/server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensure-authenticated';
 import { AppointmentsController } from '../controllers/appointments-controller';
+import { ProviderAppointmentsController } from '../controllers/provider-appointments-controller';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export { appointmentsRouter };
