e64610acdb2816f049d585bb46326b7498947875
flexcommits <leeweiming524@gmail.com>
2021-02-05 14:59:34 -0300

    feat: add ses mail

---
+++ packages/server/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: process.env.SES_EMAIL_ADDESS,
+      name: process.env.SES_NAME,
+    },
+  },
+} as IMailConfig;
--- packages/server/src/shared/container/providers/index.ts
+++ packages/server/src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import { IStorageProvider } from './storage-provider/models/interface-storage-provider';
 import { DiskStorageProvider } from './storage-provider/implementations/disk-storage-provider';
 
 import { IMailProvider } from './mail-provider/models/interface-mail-provider';
 import { EtherealMailProvider } from './mail-provider/implementations/ethereal-mail-provider';
+import { SesMailProvider } from './mail-provider/implementations/ses-mail-provider';
 
 import { IMailTemplateProvider } from './mail-template-provider/models/interface-mail-template-provider';
 import { HandlebarsMailTemplateProvider } from './mail-template-provider/implementations/handlebars-mail-template-providers';
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SesMailProvider),
 );
