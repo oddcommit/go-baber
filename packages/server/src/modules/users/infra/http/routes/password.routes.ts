35ca0d80f65f457d0cd75527c195e3c2acf2b0e9
flexcommits <leeweiming524@gmail.com>
2021-02-05 11:25:28 -0300

    feat: add validations with celebrate and Joi

--- packages/server/src/modules/users/infra/http/routes/password.routes.ts
+++ packages/server/src/modules/users/infra/http/routes/password.routes.ts
@@ -1,13 +1,33 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import { ForgotPasswordController } from '../controllers/forgot-password-controller';
 import { ResetPasswordController } from '../controllers/reset-password-controller';
 
 const passwordRouter = Router();
+
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/reset', resetPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export { passwordRouter };
--- packages/server/src/modules/users/infra/http/routes/sessions.routes.ts
+++ packages/server/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import { SessionsController } from '../controllers/sessions-controller';
 
 const sessionsRouter = Router();
 const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', sessionsController.create);
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create,
+);
 
 export { sessionsRouter };
